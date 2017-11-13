6ca25022741d295b1601b5782155026423ded4db
Kendra Moore <kendra.j.moore3443@gmail.com>
2017-11-12 19:47:54 -0800

    Work on user resource

--- controller/users.js
+++ controller/users.js
@@ -6,91 +6,93 @@ const Trip = require('../models/trip.js');
 
 module.exports = (app) => {
   // API INDEX ROUTE
-  app.get('/trips', function (req, res) {
-    Trip.find(function(err, trips) {
-      if (err) {
-        console.log(err)
-      }
-        res.send({trips: trips});
-    })
-  })
+  // app.get('/login', function (req, res) {
+  //   User.find(function(err, user) {
+  //     if (err) {
+  //       console.log(err)
+  //     }
+  //       res.send({user: user});
+  //   })
+  // })
+  //
+  //
 
-  // WEB INDEX
+
   // app.get('/', function (req, res) {
-  //   const now = new Date();
-  //   Trip.find({ departsOn: { $gt: now } }).sort('departsOn desc').exec(function(err, trips) {
+  //   const user = new User;
+  //   User.find({ departsOn: { $gt: now } }).sort('departsOn desc').exec(function(err, user) {
   //     if (err) { return console.log(err) }
-  //      res.render('trips-index', {trips: trips});
+  //      res.render('/login', { user: user });
   //   })
   // })
 
-  // NEW
+  //NEW
   app.get('/sign-up', function (req, res) {
     res.render('sign-up', { user: user });
   })
 
   //CREATE
-  app.post('/sign-up', function (req, res) {
-    User.create(req.body, function(err, user) {
-      if (req.header('Content-Type') == 'application/json') {
-        if (err) {
-          console.log(err)
-          return res.status(400).send({ message: "There was a problem creating your trip."})
-        }
-        return res.send({ trip: trip }); //=> RETURN JSON
-      } else {
-        if (err) {
-          console.log(err)
-          return res.redirect('/trips/new')
-        }
-        return res.redirect('/trips/' + trip._id);
-      }
-    })
-  })
+  // app.post('/sign-up', function (req, res) {
+  //   User.create(req.body, function(err, user) {
+  //     if (req.header('Content-Type') == 'application/json') {
+  //       if (err) {
+  //         console.log(err)
+  //         return res.status(400).send({ message: "There was a problem creating your trip."})
+  //       }
+  //       return res.send({ user: user }); //=> RETURN JSON
+  //     } else {
+  //       if (err) {
+  //         console.log(err)
+  //         return res.redirect('/sign-up')
+  //       }
+  //       return res.redirect('/login');
+  //     }
+  //   })
+  // })
 
   // SHOW
-  app.get('/trips/:id', function (req, res) {
-    Trip.findById(req.params.id).exec(function (err, trip) {
-      if (req.header('Content-Type') == 'application/json') {
-        return res.send({ trip: trip }); //=> RETURN JSON
-      } else {
-        return res.render('trips-show', {trip: trip});
-      }
-    })
-  })
+  // app.get('/trips/:id', function (req, res) {
+  //   Trip.findById(req.params.id).exec(function (err, trip) {
+  //     if (req.header('Content-Type') == 'application/json') {
+  //       return res.send({ trip: trip }); //=> RETURN JSON
+  //     } else {
+  //       return res.render('trips-show', {trip: trip});
+  //     }
+  //   })
+  // })
 
   //UPDATE
-  app.put('/trips/:id', function (req, res) {
-    req.body.departsOn = new Date(req.body.departsOn + " PST")
-    req.body.returnsOn = new Date(req.body.returnsOn + " PST")
-
-    Trip.findByIdAndUpdate(req.params.id,  req.body, function(err, trip) {
-      if (err) { return console.log(err) }
-      res.redirect('/trips/' + trip._id);
-    })
-  })
+  // app.put('/trips/:id', function (req, res) {
+  //   req.body.departsOn = new Date(req.body.departsOn + " PST")
+  //   req.body.returnsOn = new Date(req.body.returnsOn + " PST")
+  //
+  //   Trip.findByIdAndUpdate(req.params.id,  req.body, function(err, trip) {
+  //     if (err) { return console.log(err) }
+  //     res.redirect('/trips/' + trip._id);
+  //   })
+  // })
 
   // EDIT
-  app.get('/trips/:id/edit', function (req, res) {
-    Trip.findById(req.params.id, function(err, trip) {
-      if (err) { return console.log(err) }
-      res.render('trips-edit', { trip: trip, timesOfDay: Trip.timesOfDay() });
-    })
-  })
+  // app.get('/trips/:id/edit', function (req, res) {
+  //   Trip.findById(req.params.id, function(err, trip) {
+  //     if (err) { return console.log(err) }
+  //     res.render('trips-edit', { trip: trip, timesOfDay: Trip.timesOfDay() });
+  //   })
+  // })
 
-  // DELETE
-  app.delete('/trips/:id', function (req, res) {
-    console.log('hello')
-    Trip.findByIdAndRemove(req.params.id, function(err) {
-      if (err) { return console.log(err) }
-      console.log('hello 2')
-      if (req.header('Content-Type') == 'application/json') {
-        return res.send({"message": "Trip deleted sucessfully"}).status(200) //=> RETURN JSON
-      } else {
-        return res.redirect('/');
-      }
-    })
-  })
+  // // DELETE
+  // app.delete('/trips/:id', function (req, res) {
+  //   console.log('hello')
+  //   Trip.findByIdAndRemove(req.params.id, function(err) {
+  //     if (err) { return console.log(err) }
+  //     console.log('hello 2')
+  //     if (req.header('Content-Type') == 'application/json') {
+  //       return res.send({"message": "Trip deleted sucessfully"}).status(200) //=> RETURN JSON
+  //     } else {
+  //       return res.redirect('/');
+  //     }
+  //   })
+  // })
 
 
 };
--- models/user.js
+++ models/user.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose'),
     Schema = mongoose.Schema;
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const UserSchema = new Schema({
     firstName       : { type: String },
     lastName        : { type: String },
@@ -9,34 +9,34 @@ const UserSchema = new Schema({
     password        : { type: String },
     phone           : { type: Number }
 });
-
-UserSchema.pre('save', function(next){
-  // SET createdAt AND updatedAt
-  const now = new Date();
-  this.updatedAt = now;
-  if ( !this.createdAt ) {
-    this.createdAt = now;
-  }
-
-  // ENCRYPT PASSWORD
-  const user = this;
-  if (!user.isModified('password')) {
-    return next();
-  }
-  bcrypt.genSalt(10, function(err, salt) {
-    bcrypt.hash(user.password, salt, function(err, hash) {
-
-      user.password = hash;
-      next();
-    });
-  });
-});
-
-
-UserSchema.methods.comparePassword = function(password, done) {
-  bcrypt.compare(password, this.password, function(err, isMatch) {
-    done(err, isMatch);
-  });
-};
+// 
+// UserSchema.pre('save', function(next){
+//   // SET createdAt AND updatedAt
+//   const now = new Date();
+//   this.updatedAt = now;
+//   if ( !this.createdAt ) {
+//     this.createdAt = now;
+//   }
+//
+//   // ENCRYPT PASSWORD
+//   const user = this;
+//   if (!user.isModified('password')) {
+//     return next();
+//   }
+//   bcrypt.genSalt(10, function(err, salt) {
+//     bcrypt.hash(user.password, salt, function(err, hash) {
+//
+//       user.password = hash;
+//       next();
+//     });
+//   });
+// });
+//
+//
+// UserSchema.methods.comparePassword = function(password, done) {
+//   bcrypt.compare(password, this.password, function(err, isMatch) {
+//     done(err, isMatch);
+//   });
+// };
 
 module.exports = mongoose.model('User', UserSchema);
